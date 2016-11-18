af1adf5030866dc16d1b224db870c01438c09295
Pablo Perotti <pablo@blackpixel.com>
2016-11-18 17:08:11 -0300

    chore: Add renamed Backup in the Cloud project to repo with name BackupInTheCloud.

---
+++ BackupInTheCloud/src/com/pabloperotti/backup/in/the/cloud/syncadapter/SyncService.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.pabloperotti.backup.in.the.cloud.syncadapter;
+
+import com.pabloperotti.backup.in.the.cloud.MyApp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * Service to handle Account sync. This is invoked with an intent with action
+ * ACTION_AUTHENTICATOR_INTENT. It instantiates the syncadapter and returns its
+ * IBinder.
+ */
+public class SyncService extends Service {
+
+    private static final Object sSyncAdapterLock = new Object();
+
+    private static SyncAdapter sSyncAdapter = null;
+
+    @Override
+    public void onCreate() {
+    	Log.i(MyApp.TAG, "SyncService.onCreate()");
+        synchronized (sSyncAdapterLock) {
+            if (sSyncAdapter == null) {
+                sSyncAdapter = new SyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+    	Log.i(MyApp.TAG, "SyncService.onBind()");
+        return sSyncAdapter.getSyncAdapterBinder();
+    }
+}
