dce12a8cc62f04130c07cec97e84f393f8aeea5b
Pablo Perotti <pablo.perotti@gmail.com>
2025-02-10 16:26:53 -0300

    Remove MoviesCatalogApp

--- MoviesCatalogApp/app/src/main/java/com/pperotti/android/moviescatalogapp/MovieApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.pperotti.android.moviescatalogapp
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MovieApplication : Application()
--- MoviesCatalogApp/app/src/main/java/com/pperotti/android/moviescatalogapp/data/common/RepositoryResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.pperotti.android.moviescatalogapp.data.common
-
-/**
- * Generic Response Wrapper that can be reused by more than one repo.
- */
-sealed class RepositoryResponse<T> {
-    data class Success<T>(val result: T) : RepositoryResponse<T>()
-    data class Error<T>(val message: String?, val cause: Throwable?) : RepositoryResponse<T>()
-}
